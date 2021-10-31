89e0f87292ad5afd8f0db3b4e26bef881dc77549
Matias Moreno <matiasmorenog@gmail.com>
2021-10-30 21:30:59 -0300

    documentacion de clases

--- src/coTest.js
+++ src/coTest.js
@@ -1,42 +1,84 @@
 class Product {
+  /**
+   * @param {String} name nombre del producto
+   * @param {Number} sellIn dias restantes para expirar
+   * @param {Number} price precio del producto
+   */
   constructor(name, sellIn, price) {
     this.name = name;
     this.sellIn = sellIn;
     this.price = price;
   }
+  /**
+   * producto expirado
+   * @return {Boolean}
+   */
   get isExpired() {
     return this.sellIn < 0;
   }
+  /**
+   * precio limite del producto
+   * @return {Number}
+   */
   get limitPrice() {
     return 50;
   }
+  /**
+   * getter llego al precio limite
+   * @return {Boolean}
+   */
   get isExpensive() {
     return this.price == this.limitPrice;
   }
+  /**
+   * getter sin precio
+   * @return {Boolean}
+   */
   get isFree() {
     return this.price == 0;
   }
+  /**
+   * setea el precio limite
+   */
   setLimitPrice() {
     this.price = this.limitPrice;
   }
+  /**
+   * incrementa el precio del producto
+   * @param {Number} v valor a incrementar - default 1
+   */
   incPrice(v = 1) {
     this.price += v;
 
     if (this.price > this.limitPrice) this.setLimitPrice();
   }
+  /**
+   * decrementa el precio del producto
+   * @param {Number} v valor a decrementar - default 1
+   */
   decPrice(v = 1) {
     this.price -= v;
 
     if (this.price < 0) this.price = 0;
   }
+  /**
+   * decrementa dias restantes
+   */
   decSellIn() {
     this.sellIn--;
   }
 }
 class CarInsurance {
+  /**
+   * @param {Array(Product)} products array de productos
+   */
   constructor(products = []) {
     this.products = products;
   }
+  /**
+   * actualiza el precio de los productos
+   * @returns {Array(Product)} array de productos
+   */
   updatePrice() {
     const products = this.products;
 
